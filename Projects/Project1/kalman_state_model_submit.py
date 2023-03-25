9a6f3b48414948e8c63d5cd15001c15ea9103bbc
ChrisKelton <ckelton@ufl.edu>
2023-03-25 17:45:22 -0400

    get kalman filter working. updates for more generalizability & updates to plotting

--- Projects/Project1/kalman_state_model_submit.py
+++ Projects/Project1/kalman_state_model_submit.py
@@ -57,11 +57,11 @@ def initial_state_vector(
 
 
 def kalman_state_model(
-    Q_k: Callable,
-    F_k: Callable,
-    G_k: Callable,
+    Q_k: Union[np.ndarray, Callable],
+    F_k: Union[np.ndarray, Callable],
+    G_k: Union[np.ndarray, Callable],
     C: np.ndarray,
-    R_k: Callable,
+    R_k: Union[np.ndarray, Callable],
     m_0: List[float],
     PI_0: np.ndarray,
     n_vars_to_solve: int,
@@ -73,6 +73,15 @@ def kalman_state_model(
         rng = np.random.default_rng(seed)
     else:
         rng = np.random.default_rng()
+
+    if not callable(Q_k):
+        Q_k = lambda k: Q_k
+    if not callable(F_k):
+        F_k = lambda k: F_k
+    if not callable(G_k):
+        G_k = lambda k: G_k
+    if not callable(R_k):
+        R_k = lambda k: R_k
 
     if m_0 is None:
         m_0 = [0]
@@ -93,7 +102,7 @@ def kalman_state_model(
         w = generate_avg_random_vector_series_from_covariance_mat(
             R_k(k - 1), samples=1, iterations=rvg_iterations, rng=rng
         )
-        print(f"k = '{k}'")
+        # print(f"k = '{k}'")
         s_k = np.expand_dims(F_k(k) @ x_k[k - 1], 1)
 
         if G_k(k).shape[1] == u.shape[0]:
@@ -111,11 +120,11 @@ def kalman_state_model(
                 f"'{u}'"
             )
 
-        print(f"x_k[k] = '{x_k[k]}'")
+        # print(f"x_k[k] = '{x_k[k]}'")
 
         temp_r_k = C @ x_k[k]
         r_k[k] = np.squeeze(np.add(np.column_stack(temp_r_k[:n_obs_to_retain]).T, w.T[:n_obs_to_retain]).T)
-        print(f"r_k[k] = '{r_k[k]}'\n\n")
+        # print(f"r_k[k] = '{r_k[k]}'\n\n")
 
     return x_k, r_k
 
@@ -127,8 +136,8 @@ def roughly_constant_velocity_motion_model(
     period: float = 1,
     acceleration_variance: Optional[float] = None,
     variance_R_k: Optional[float] = None,
-    Q_k: Optional[Callable] = None,
-    R_k: Optional[Callable] = None,
+    Q_k: Optional[Union[np.ndarray, Callable]] = None,
+    R_k: Optional[Union[np.ndarray, Callable]] = None,
     iterations: int = 100,
     rvg_iterations: int = 1000,
     seed: Optional[int] = None,
@@ -157,6 +166,8 @@ def roughly_constant_velocity_motion_model(
 
     if R_k is None:
         R_k = lambda k: np.eye(2) * np.asarray((variance_R_k, 1))
+    elif not callable(R_k):
+        R_k = lambda k: R_k
 
     F_k = lambda k: np.array(([[1, period], [0, 1]]))
     if retain_all_obs:
@@ -171,6 +182,8 @@ def roughly_constant_velocity_motion_model(
         Q_k = lambda k: acceleration_variance * np.array(
             [[(period ** 4) / 4, (period ** 3) / 2], [(period ** 3) / 2, period ** 2]]
         )
+    elif not callable(Q_k):
+        Q_k = lambda k: Q_k
 
     x_k, r_k = kalman_state_model(
         Q_k=Q_k,
--- Projects/Project2/kalman_state_filter.py
+++ Projects/Project2/kalman_state_filter.py
@@ -3,6 +3,7 @@ from typing import Optional, Callable, Tuple, List, Union
 
 import matplotlib.pyplot as plt
 import numpy as np
+from time import time_ns
 
 from kalman_state_model_submit import roughly_constant_velocity_motion_model
 
@@ -15,31 +16,41 @@ def plot_kalman_filter_estimates_vs_observations_and_ground_truth(
     xticks: Optional[Union[list, np.ndarray]] = None,
 ):
     x = np.arange(0, len(observations))
-    plt.plot(x, ground_truth, label="truth")
-    plt.plot(x, observations, "-.", label="observations")
-    plt.plot(x, estimates, "--", label="estimates")
-    plt.title("Kalman Filter Discrete-Time, Time-Invariant", fontsize=9)
-    plt.xlabel("Time (s)")
+    fig, ax = plt.subplots(2, 1, figsize=(10, 10))
+    ax[0].plot(x, ground_truth[:, 0], label="truth")
+    ax[0].plot(x, observations[:, 0], "-.", label="observations")
+    ax[0].plot(x, estimates[:, 0], "--", label="estimates")
+    ax[0].set_title("Kalman Filter Discrete-Time, Time-Invariant", fontsize=9)
+    ax[0].set_xlabel("Time (s)")
     if xticks is None:
         xticks = x
     xtick_positions = np.linspace(0, len(x), len(xticks))
-    plt.xticks(xtick_positions, xticks)
-    plt.ylabel("Position (m)")
-    plt.legend(["truth", "observations", "estimates"])
+    ax[0].set_xticks(xtick_positions, xticks)
+    ax[0].set_ylabel("Position (m)")
+    ax[0].legend(["truth", "observations", "estimates"])
 
-    plt.tight_layout()
-    plt.savefig(fig_output_path)
+    ax[1].plot(x, ground_truth[:, 1], label="truth")
+    ax[1].plot(x, observations[:, 1], "-.", label="observations")
+    ax[1].plot(x, estimates[:, 1], "--", label="estimates")
+    ax[1].set_title("Kalman Filter Discrete-Time, Time-Invariant", fontsize=9)
+    ax[1].set_xlabel("Time (s)")
+    ax[1].set_xticks(xtick_positions, xticks)
+    ax[1].set_ylabel("Velocity (m/s)")
+    ax[1].legend(["truth", "observations", "estimates"])
+
+    fig.tight_layout()
+    fig.savefig(fig_output_path)
     plt.close()
 
 
 def plot_kalman_filter_kalman_gain_position_and_velocity(
-    K: np.ndarray,  # K[0] = position, K[1] = velocity
+    K: np.ndarray,
     fig_output_path: Path,
     xticks: Optional[Union[list, np.ndarray]] = None,
 ):
-    x = np.arange(0, K.shape[1])
+    x = np.arange(0, len(K))
     fig, ax = plt.subplots(2, 1, figsize=(10, 10))
-    ax[0].plot(x, K[0])
+    ax[0].plot(x, K[:, 0])
     ax[0].set_title("Kalman Gain Position")
     ax[0].set_xlabel("Time (s)")
     if xticks is None:
@@ -49,7 +60,7 @@ def plot_kalman_filter_kalman_gain_position_and_velocity(
     ax[0].set_ylabel("Gain")
     ax[0].set_ylim([0, 1.5])
 
-    ax[1].plot(x, K[1])
+    ax[1].plot(x, K[:, 1])
     ax[1].set_title("Kalman Gain Velocity")
     ax[1].set_xlabel("Time (s)")
     ax[1].set_xticks(xtick_positions, xticks)
@@ -62,13 +73,13 @@ def plot_kalman_filter_kalman_gain_position_and_velocity(
 
 
 def plot_kalman_filter_estimate_covariance_diagonals(
-    P: np.ndarray,  # P[0] = diagonal elements of position, P[1] = diagonal elements of velocity
+    P: np.ndarray,
     fig_output_path: Path,
     xticks: Optional[Union[list, np.ndarray]] = None,
 ):
-    x = np.arange(0, P.shape[1])
+    x = np.arange(0, len(P))
     fig, ax = plt.subplots(2, 1, figsize=(10, 10))
-    ax[0].plot(x, P[0])
+    ax[0].plot(x, P[:, 0])
     ax[0].set_title("Estimate Covariance Position Diagonals")
     ax[0].set_xlabel("Time (s)")
     if xticks is None:
@@ -78,7 +89,7 @@ def plot_kalman_filter_estimate_covariance_diagonals(
     ax[0].set_ylabel("MSE (Position)")
     ax[0].set_yscale("log")
 
-    ax[1].plot(x, P[1])
+    ax[1].plot(x, P[:, 1])
     ax[1].set_title("Estimate Covariance Velocity Diagonals")
     ax[1].set_xlabel("Time (s)")
     ax[1].set_xticks(xtick_positions, xticks)
@@ -137,10 +148,12 @@ def kalman_filter(
 
         # x^(k) = x^(k|k-1) + K(k) [r(k) - C x^(k|k-1)]
         # update state estimate
-        x_estimator[k] = np.add(x_est_lag, (K[k-1].dot(np.subtract(np.column_stack(observations[k]), C.dot(x_est_lag)))).T)
+        # x_estimator[k] = np.add(x_est_lag, (K[k-1].dot(np.subtract(np.column_stack(observations[k]), C.dot(x_est_lag)))).T)
+        x_estimator[k] = np.add(x_est_lag, (K[k-1].dot(np.subtract(np.row_stack(observations[k]), C.dot(x_est_lag)))))
         # x_estimator[k] = np.add(x_est_lag, np.expand_dims(K[k-1].dot(np.subtract(np.column_stack(observations[k]), C.dot(x_est_lag)))[-1, :], 0).T)
 
-    return x_estimator, P, K
+    # get estimates in same form as observations, e.g. obs[0] = [0, 0], est[0] = [[0], [0]] -> est[0, 0]
+    return np.column_stack(x_estimator).T, P, K
 
 
 def roughly_constant_velocity_motion_filter(
@@ -152,14 +165,16 @@ def roughly_constant_velocity_motion_filter(
     period: float = 1,
     acceleration_variance: Optional[float] = None,
     variance_R_k: Optional[float] = None,
-    Q_k: Optional[Callable] = None,
-    R_k: Optional[Callable] = None,
+    Q_k: Optional[Union[np.ndarray, Callable]] = None,
+    R_k: Optional[Union[np.ndarray, Callable]] = None,
     n_observations: int = 100,
     rvg_iterations: int = 100,
     seed: Optional[int] = None,
     fig_output_path: Optional[Path] = None,
 ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     # p_k = ground truth position, s_k = ground truth position
+    t0 = time_ns()
+    print("*** Generating Kalman State Model Observations ***")
     observations, Q_k, F_k, G_k, C, R_k, m_0, PI_0, p_k, s_k = roughly_constant_velocity_motion_model(
         p0=p0,
         s0=s0,
@@ -175,7 +190,11 @@ def roughly_constant_velocity_motion_filter(
         fig_output_path=fig_output_path,
         retain_all_obs=True,
     )
+    t1 = time_ns()
+    print(f"Time taken -- {(t1 - t0)/1e9:.3f}s\n")
 
+    t0 = time_ns()
+    print("*** Estimating signal using observations ***")
     x_estimator, P, K = kalman_filter(
         initial_state_estimate=initial_state_estimate,
         P_initial=P_initial,
@@ -186,8 +205,11 @@ def roughly_constant_velocity_motion_filter(
         C=C,
         R_k=R_k,
     )
+    t1 = time_ns()
+    print(f"Time taken -- {(t1 - t0)/1e9:.3f}s\n")
 
     if fig_output_path is not None:
+        print("*** Plotting ***")
         total_time = n_observations * period
         xticks = np.arange(0, total_time + 1)
         if Path(fig_output_path).is_dir():
@@ -198,20 +220,20 @@ def roughly_constant_velocity_motion_filter(
             fig_output_path.parent.mkdir(exist_ok=True, parents=True)
         plot_kalman_filter_estimates_vs_observations_and_ground_truth(
             observations=observations,
-            estimates=x_estimator.T[0][0],
-            ground_truth=p_k,
+            estimates=x_estimator,
+            ground_truth=np.column_stack((p_k, s_k)),
             fig_output_path=temp_path,
             xticks=xticks,
         )
         temp_path = temp_path.parent / "kalman_filter_gain.png"
         plot_kalman_filter_kalman_gain_position_and_velocity(
-            K=np.squeeze(K.T),
+            K=np.diagonal(K, axis1=1, axis2=2),  # get diagonals of Kalman Gain in the form of [position_diag, velocity_diag]
             fig_output_path=temp_path,
             xticks=xticks[1:],
         )
         temp_path = temp_path.parent / "kalman_filter_estimate_covariance_diagonals.png"
         plot_kalman_filter_estimate_covariance_diagonals(
-            P=np.diagonal(P, axis1=1, axis2=2).T,
+            P=np.diagonal(P, axis1=1, axis2=2),
             fig_output_path=temp_path,
             xticks=xticks,
         )
@@ -242,7 +264,6 @@ def main():
         seed=seed,
         fig_output_path=fig_output_path,
     )
-    a = 0
 
 
 if __name__ == '__main__':
